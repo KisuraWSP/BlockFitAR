e86812d876425f648e44f35a7c77f49d40d8b03a
KisuraWSP <kisura2003@gmail.com>
2025-10-13 20:57:48 +0530

    Initial Commit

---
+++ BlockFitAR/BlockFitARApp.swift
@@ -0,0 +1,21 @@
+//
+//  BlockFitARApp.swift
+//  BlockFitAR
+//
+//  Created by Kisura W.S.P on 2025-10-13.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct BlockFitARApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
